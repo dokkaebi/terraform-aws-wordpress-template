cf9d74de3c8ded90f3771dd3d5c9f6303880605b
Austin Lally <adlally@gmail.com>
2023-03-18 21:05:50 -0700

    upgrade provider versions

--- versions.tf
+++ versions.tf
@@ -5,11 +5,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.62.0"
+      version = ">= 3.62.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.5.1"
+      version = ">= 2.5.1"
     }
   }
 }
